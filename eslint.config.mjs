cafa2080df947814758bae106e179d9885c95ada
Tako <tako.renraku1@gmail.com>
2025-03-07 11:59:40 +0900

    Update eslint.config and Add eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier"
+  ),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Next.jsでは不要
+      "@typescript-eslint/explicit-module-boundary-types": "off", // TSの型推論に任せる
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // 未使用の変数に警告
+      "jsx-a11y/anchor-is-valid": "off" // Next.jsのLinkでは不要
+    }
+  }
 ];
 
 export default eslintConfig;

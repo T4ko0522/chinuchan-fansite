40d8efd177711684fae51079df248772a8eb6137
Tako <tako.renraku1@gmail.com>
2025-03-09 03:25:33 +0900

    update prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "es5",
+    printWidth: 100,
+  }
+    
